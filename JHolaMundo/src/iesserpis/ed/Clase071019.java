af4c932df142605f1a263c99cc56d7a5623ad665
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-07 13:50:12 +0200

    Clase dia 7/10/19

---
+++ JHolaMundo/src/iesserpis/ed/Clase071019.java
@@ -0,0 +1,22 @@
+package iesserpis.ed;
+
+import java.util.Scanner;
+
+public class Clase071019 {
+
+	
+	// InteligenciaAdivina
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+			
+		int i, n=0, rand;
+		boolean esVerdadero = false;
+		
+		
+		
+		
+		
+	}
+
+}
