5a482ab118f6a54267403a96ce3b99d19fe865b3
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-07 13:56:36 +0200

    Programa facil con While

--- JHolaMundo/src/iesserpis/ed/Clase071019.java
+++ JHolaMundo/src/iesserpis/ed/Clase071019.java
@@ -10,13 +10,17 @@ public class Clase071019 {
 		
 		Scanner scanner = new Scanner(System.in);
 			
-		int i, n=0, rand;
+		int i, n=0, rand, numero=1;
 		boolean esVerdadero = false;
 		
-		
+		while (numero != 0){
+			
+			System.out.print("introduce un numeri (0 para terminar): ");
+			numero = scanner.nextInt();
+		}
+		System.out.println("Fin del programa");
 		
 		
 		
 	}
-
 }
