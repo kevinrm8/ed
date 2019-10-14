110f61ae637b49d5c5ca2220ef391e74ce111e03
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-14 14:02:18 +0200

    Array

--- JSuma/src/Adivina.java
+++ JSuma/src/Adivina.java
@@ -2,8 +2,9 @@
 public class Adivina {
 
 	public static void main(String[] args) {
-		int[] v= {17,15};
+		int[] v= {17,15,3,69,8};
 	
-	System.out.println("Posicion 1 del array vale "+v[1]);
+		for (int i=0;i<v.length;i++) System.out.println("v["+i+ "]="+v[i]);
+	// for(int i=0;i<5;i++) System.out.prinf("v[%s] =%s%n" ,i,v[i]);
 	}
 }
