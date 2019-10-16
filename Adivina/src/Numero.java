b91786c945421f669cc1b8ae2253fc1f4b171a24
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-16 23:53:59 +0200

    Adivina numero

--- Adivina/src/Numero.java
+++ Adivina/src/Numero.java
@@ -4,30 +4,21 @@ public class Numero {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int num=0,aleatorio,i,contador=0;
+		int num,aleatorio,contador=1,i;
 		
-		System.out.println("Intenta acertar el numero entre [1-10]");
+		aleatorio = (int) (Math.random()*10+1);
+		
+		do { 
+			System.out.println("Adivina el numero entre [1-10]");
+			num= scanner.nextInt();
 			
+				if(num != aleatorio) {
+					System.out.println("Has fallado!");
+					contador++;}
+							
+			} while (num != aleatorio);
+			System.out.println("Has acertado!Solo te ha costado "+contador+" intentos.");
 		
-		aleatorio=(int) (Math.random()*10); //Creo numero aleatorio
 		
-		if(num != aleatorio) { //Primera vez que intento acertar numero
-			System.out.println("Prueba suerte: ");
-			num = scanner.nextInt();
-					
-			for(i=1;i<=10;i++) {
-					if (num != aleatorio) {
-						System.out.println("Has fallado! Vuelve a intentarlo: ");
-						num = scanner.nextInt();
-											}
-						if(num==aleatorio) {
-							i=11;
-											}
-							else {
-								System.out.println("Enhrabuena! Has acertado!");
-								}
-								}
-							}
-		
+		}
 	}
-}
