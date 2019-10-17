540a4c927b1cf426cc089bd34de2f47b85e22d33
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-17 08:30:35 +0200

    Borrar archivos

--- Eclipse-Work/ADIVINA/src/Anumero.java
+++ Eclipse-Work/ADIVINA/src/Anumero.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class Anumero {
 
 	public static void main(String[] args) {
-		// (Math.random()*numMax+1);
-
 		Scanner scanner = new Scanner(System.in);
+		int num,aleatorio,contador=1,i;
+		
+		aleatorio = (int) (Math.random()*10+1);
+		
+		do { 
+			System.out.println("Adivina el numero entre [1-10]");
+			num= scanner.nextInt();
+			
+				if(num != aleatorio) {
+					System.out.println("Has fallado!");
+					contador++;}
+							
+			} while (num != aleatorio);
+			System.out.println("Has acertado!Solo te ha costado "+contador+" intentos.");
 		
 		
-		
+		}
 	}
-
-}
