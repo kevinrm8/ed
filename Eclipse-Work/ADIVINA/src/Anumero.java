b6a34e3abd34fd0a6fef119940727985984f6515
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-17 10:33:09 +0200

    Mejorando Adivina

--- Eclipse-Work/ADIVINA/src/Anumero.java
+++ Eclipse-Work/ADIVINA/src/Anumero.java
@@ -6,30 +6,40 @@ public class Anumero {
 		Scanner scanner = new Scanner(System.in);
 		
 		
-		int num,aleatorio,contador=1,i,intentos=3;
-		
-		aleatorio = (int) (Math.random()*10+1);
+		int num,aleatorio,contador,i,intentos=3,n=1,partida=1;
 		
 		
 		
-		do { 
-			System.out.println("Adivina el numero entre [1-10]");
-			num= scanner.nextInt();
-			
-				if(num != aleatorio) {
-					System.out.println("Has fallado!");
-						if(num>aleatorio){
-						System.out.println("Te has pasado! El numero que buscas es menor que "+num);
-						}
-						else System.out.println("Te has quedado corto! El numero que buscas es mayor que "+num);
-					contador++;
-									}
-							
-			} while (num != aleatorio);
-				if ( num == aleatorio);
-				System.out.println("Has acertado!Solo te ha costado "+contador+" intentos.");
+		
+		while (n != 0){
+			aleatorio = (int) (Math.random()*10+1);
+			contador=1;
+			do { 
+				System.out.println("Adivina el numero entre [1-10]");
+				num= scanner.nextInt();
+				
+					if(num != aleatorio) {
+						System.out.println("Has fallado!");
+							if(num>aleatorio){
+							System.out.println("Te has pasado! El numero que buscas es menor que "+num);
+							}
+							else System.out.println("Te has quedado corto! El numero que buscas es mayor que "+num);
+						contador++;
+										}
+								
+				} while (num != aleatorio);
+					if ( num == aleatorio);
+					System.out.println("Has acertado!Te ha costado un total de "+contador+" intentos.");
 
-				}
+			
+							System.out.println("Quieres jugar otra partida? (No=0/Si=1)");
+							n = scanner.nextInt();
+							if(n==1)
+								partida ++;
+						
+							}
+		System.out.println("Has jugado un total de "+partida+" partidas");
+	}
 }
 	
 
