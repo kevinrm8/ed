dc900c3a27d623ed72c39598002a46e5bb0e9c15
kevinrm8 <kevmar@alu.iesserpis.org>
2019-10-17 08:23:55 +0200

    Dia anterior de clase

---
+++ Eclipse-Work/ADIVINA/src/Anumero.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Anumero {
+
+	public static void main(String[] args) {
+		// (Math.random()*numMax+1);
+
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
--- Eclipse-Work/JSuma/src/Array.java
+++ Eclipse-Work/JSuma/src/Array.java
@@ -1,5 +1,5 @@
 
-public class Adivina {
+public class Array {
 
 	public static void main(String[] args) {
 		int[] v= {17,15,3,69,8};
@@ -7,4 +7,6 @@ public class Adivina {
 		for (int i=0;i<v.length;i++) System.out.println("v["+i+ "]="+v[i]);
 	// for(int i=0;i<5;i++) System.out.prinf("v[%s] =%s%n" ,i,v[i]);
 	}
+	
+
 }
